5584459b325c5c24868fcd6841f60171272022ca
diggi <diggi@DESKTOP-89T2QB5>
2025-10-09 17:17:56 +0100

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Personal_Finance_Tracker
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
