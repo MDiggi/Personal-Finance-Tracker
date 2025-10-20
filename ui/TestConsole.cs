e6b667bc2292c6a557bf29dba6a044e3b46f9df0
Dinis Costa <dinisca12@gmail.com>
2025-10-20 15:11:43 +0100

    aaa

--- ui/TestConsole.cs
+++ ui/TestConsole.cs
@@ -30,7 +30,7 @@ namespace Personal_Finance_Tracker.ui
                         adminConsole.Show();
                         break;
                     case '0':
-                        Console.WriteLine("\nExiting Test Console...");
+                        Console.WriteLine("\ndick");
                         return;
                     default:
                         Console.WriteLine("\nInvalid Option. Please try again.");
